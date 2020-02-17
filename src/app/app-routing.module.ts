6fd559d7ede71e2831806c87159b60e605c37e90
andres vega <andres.vega@symplifica.com>
2020-02-17 16:37:06 -0500

    Ajustes iniciales - se crean componentes basicos - se crean las paginas de la app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: PortafolioComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'item', component: ItemComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
